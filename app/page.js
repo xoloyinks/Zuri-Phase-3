d8fd00cdb2d321f8509d13d1d1072781c1428ca2
Kolawole Moses <xoloyinks@gmail.com>
2023-09-21 19:38:39 -0700

    Updated

--- app/page.js
+++ app/page.js
@@ -5,12 +5,12 @@ import { getServerSession } from 'next-auth'
 
 
 export default function Home() {
-  const session = await getServerSession(options);
+  
   return (
     <>
-      {session ? (
-        <Gallery /> : <h1 className='text-4xl'>Page not available for unauthenticated user</h1>
-      )}
+      <section>
+        <Gallery />
+      </section>
     </>
   )
 }
